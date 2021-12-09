302d94156ebd4b9074c7cbffbe863997879c0ad9
Rüdiger Ludwig <github@savinien.de>
2021-12-09 17:29:20 +0100

    Improved: Better split sets

--- advent/common/utils.py
+++ advent/common/utils.py
@@ -23,6 +23,10 @@ def read_data(day: int, file_name: str) -> Iterator[str]:
                 return
 
 
-def split_set(orig: set[T], pred: Callable[[T], bool]) -> tuple[set[T], set[T]]:
-    matches = {item for item in orig if pred(item)}
-    return matches, (orig - matches)
+def split_set(full_set: set[T], predicate: Callable[[T], bool]) -> tuple[set[T], set[T]]:
+    """ Splits a set in two sorted by the predicate """
+    true_set: set[T] = set()
+    false_set: set[T] = set()
+    for item in full_set:
+        (true_set if predicate(item) else false_set).add(item)
+    return true_set, false_set
